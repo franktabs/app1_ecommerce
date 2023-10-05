69ba6e5165639a602d176a3a11423c44174aa791
franktabs <tabuguiafrank@gmail.com>
2023-10-06 01:52:41 +0200

    hello world

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app-old/app-old.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
