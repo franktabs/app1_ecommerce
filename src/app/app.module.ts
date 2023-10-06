f13c9143d76b5cab3386d2fff2758782af2a006d
franktabs <tabuguiafrank@gmail.com>
2023-10-06 02:54:35 +0200

    ajout composant header/components/nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { HeaderModule } from './header/header.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
